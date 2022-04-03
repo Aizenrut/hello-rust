feaeabae0ac2da73cdc34d02a0fe71a09ffa924c
Aizenrut <igorce2000@gmail.com>
2022-04-03 12:42:25 -0300

    Cached closure

---
+++ closure/src/algorithms/caching.rs
@@ -0,0 +1,85 @@
+use std::collections::HashMap;
+use std::cmp::Eq;
+use std::hash::Hash;
+
+pub struct Cacher<T, K, V> where T: FnMut(K) -> V {
+    calculation: T,
+    map: HashMap<K, V>
+}
+
+impl<T, K, V> Cacher<T, K, V>
+where
+    T: FnMut(K) -> V,
+    K: Eq + Hash + Copy,
+    V: Copy
+{
+
+    pub fn new(calculation: T) -> Cacher<T, K, V> {
+        Cacher {
+            calculation,
+            map: HashMap::new()
+        }
+    }
+
+    pub fn value(&mut self, arg: K) -> V {
+        match self.map.get(&arg) {
+            Some(v) => *v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.map.insert(arg, v);
+                v
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+pub mod tests{
+
+    use super::*;
+
+    #[test]
+    fn with_integers() {
+        let mut count = 0;
+        let mut cached_closure = Cacher::new(|x| {
+            count += 1;
+            x
+        });
+
+        assert_eq!(1, cached_closure.value(1));
+        assert_eq!(2, cached_closure.value(2));
+        assert_eq!(1, cached_closure.value(1));
+        assert_eq!(2, cached_closure.value(2));
+        assert_eq!(2, count);
+    }
+
+    #[test]
+    fn with_strings() {
+        let mut count = 0;
+        let mut cached_closure = Cacher::new(|x| {
+            count += 1;
+            x
+        });
+
+        assert_eq!("abc", cached_closure.value("abc"));
+        assert_eq!("xyz", cached_closure.value("xyz"));
+        assert_eq!("abc", cached_closure.value("abc"));
+        assert_eq!("xyz", cached_closure.value("xyz"));
+        assert_eq!(2, count);
+    }
+
+    #[test]
+    fn with_bools() {
+        let mut count = 0;
+        let mut cached_closure = Cacher::new(|x| {
+            count += 1;
+            x
+        });
+
+        assert_eq!(true, cached_closure.value(true));
+        assert_eq!(false, cached_closure.value(false));
+        assert_eq!(true, cached_closure.value(true));
+        assert_eq!(false, cached_closure.value(false));
+        assert_eq!(2, count);
+    }
+}
---
+++ closure/src/main.rs
@@ -0,0 +1,36 @@
+mod algorithms;
+
+use std::thread;
+use std::time::Duration;
+use algorithms::caching::Cacher;
+
+
+fn main() {
+    let intensity = 1;
+    let random_number = 6;
+
+    generate_workout(intensity, random_number);
+}
+
+pub fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(|x| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        x
+    });
+    
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!", expensive_result.value(intensity)
+        );
+        println!(
+            "Next, do {} situps!", expensive_result.value(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!("Today, run for {} minutes!", expensive_result.value(intensity));
+        }
+    }
+}
